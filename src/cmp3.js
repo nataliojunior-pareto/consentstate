3cdf829c4676f339823f35b0974fe8a686eb39ae
nataliojunior-pareto <natalio.junior@pareto.io>
2025-01-09 13:26:45 -0300

    Create cmp3.js

---
+++ src/cmp3.js
@@ -0,0 +1,108 @@
+const consentBanner = document.getElementById('consent-banner');
+const acceptAllButton = document.getElementById('accept-all');
+const rejectAllButton = document.getElementById('reject-all');
+const manageButton = document.getElementById('manage');
+const manageOptions = document.querySelector('.manage-options');
+const saveManageButton = document.getElementById('save-manage');
+const cancelManageButton = document.getElementById('cancel-manage');
+const consentOptionsContainer = document.querySelector('.consent-options-container');
+
+const consentOptions = [
+  'ad_storage',
+  'analytics_storage',
+  'ad_user_data',
+  'ad_personalization',
+  'personalization_storage',
+  'functionality_storage',
+  'security_storage',
+];
+
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) {
+    const encodedValue = parts.pop().split(';').shift();
+    // Decodifica a string antes de retornar:
+    return decodeURIComponent(encodedValue);
+  }
+  return null;
+}
+
+function setCookie(name, value, days) {
+  const expires = new Date();
+  expires.setTime(expires.getTime() + (days * 24 * 60 * 60 * 1000));
+  let cookie = `${name}=${encodeURIComponent(JSON.stringify(value))};expires=${expires.toUTCString()};path=/`;
+
+    // Define SameSite=None; Secure para todos os cookies, exceto em localhost para desenvolvimento
+    if (window.location.hostname !== "localhost") {
+        cookie += "; SameSite=None; Secure";
+    }
+
+  document.cookie = cookie;
+}
+
+function updateConsent(consentObject) {
+    //gtag('consent', 'update', consentObject);
+    setCookie('pareto_consent_state', consentObject, 90);
+    consentBanner.style.display = 'none';
+    setTimeout(() => {
+      dataLayer.push({
+      'consent_state': consentObject,
+      'event': 'update_consent'
+      });
+    }, 1000);
+}
+
+function defaultConsent() {
+  // Inicialização dataLayer e configurações iniciais
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    
+    gtag('consent', 'default', {
+      'ad_storage': 'denied',
+      'ad_user_data': 'denied',
+      'ad_personalization': 'denied',
+      'analytics_storage': 'denied',
+      'personalization_storage': 'denied',
+      'functionality_storage': 'denied',
+      'security_storage': 'denied',
+      'wait_for_update': 500,
+      'region': ['BR']
+    });
+}
+
+function createBanner(urlBanner) {
+    // Fetch do arquivo HTML via jsDelivr
+    return fetch(urlBanner)
+        .then(response => response.text())
+        .then(html => {
+            // Criar um elemento temporário para converter a string HTML em elemento DOM
+            const temp = document.createElement('div');
+            temp.innerHTML = html;
+            
+            // Inserir o banner no documento
+            document.body.appendChild(temp.firstElementChild);
+        })
+        .catch(error => console.error('Erro ao carregar o banner:', error));
+}
+
+function hideBanner() {
+    return null
+}
+
+function autoBlocking() {
+    const consentStateString = getCookie('pareto_consent_state');
+    if (consentStateString) {
+        consentBanner.style.display = 'none';
+        const consentState = JSON.parse(consentStateString);
+        updateConsent(consentState)
+    } else {
+        // Consentimento não dado, mostra o banner
+        consentBanner.style.display = 'flex';
+    }
+}
+
+function start() {
+  defaultConsent();
+  window.addEventListener('DOMContentLoaded', checkConsent);
+}
