2a952ec70f55a52b30575a38115403db5ac6089c
nataliojunior-pareto <natalio.junior@pareto.io>
2025-01-10 11:23:33 -0300

    Update and rename script1.js to script2.js

--- mellifluous/script2.js
+++ mellifluous/script2.js
@@ -1,10 +1,11 @@
 const url = 'https://cdn.jsdelivr.net/gh/nataliojunior-pareto/consentstate@main/mellifluous/banner-template.html';
 
-import { createBanner } from 'https://cdn.jsdelivr.net/gh/nataliojunior-pareto/consentstate@main/src/bridge1.js';
+import { createBanner, start } from 'https://cdn.jsdelivr.net/gh/nataliojunior-pareto/consentstate@main/src/bridge1.js';
 
 async function initializeCMP() {
     try {
-        await createBanner(url);
+        await start();
+        createBanner(url);
     } catch (error) {
         console.error('Erro ao inicializar CMP:', error);
     }
