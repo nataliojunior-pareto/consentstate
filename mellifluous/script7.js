e92cdb101ab6632aacf9fd15efd3a17bbd113579
nataliojunior-pareto <natalio.junior@pareto.io>
2025-01-10 13:17:13 -0300

    Update script7.js

--- mellifluous/script7.js
+++ mellifluous/script7.js
@@ -1,4 +1,4 @@
-const url = 'https://cdn.jsdelivr.net/gh/nataliojunior-pareto/consentstate@main/mellifluous/banner-template2.html';
+const url = 'https://cdn.jsdelivr.net/gh/nataliojunior-pareto/consentstate@main/mellifluous/banner-template3.html';
 
 import { createBanner, start } from 'https://cdn.jsdelivr.net/gh/nataliojunior-pareto/consentstate@main/src/bridge4.js';
 
