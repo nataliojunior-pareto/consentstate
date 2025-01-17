341e1bf6d919721f10c41c52551e3f2d6a3aef12
nataliojunior-pareto <natalio.junior@pareto.io>
2025-01-17 16:40:57 -0300

    Update and rename script43.js to script44.js

--- mellifluous/script44.js
+++ mellifluous/script44.js
@@ -1,6 +1,6 @@
 const url = 'https://cdn.jsdelivr.net/gh/nataliojunior-pareto/consentstate@main/mellifluous/banner-template24.html';
 
-import { createBanner, start, acceptAllConsents, rejectAllConsents, updateManagedConsents, autoBlocking} from 'https://cdn.jsdelivr.net/gh/nataliojunior-pareto/consentstate@main/src/bridge-minified.js';
+import { createBanner, start, acceptAllConsents, rejectAllConsents, updateManagedConsents, autoBlocking} from 'https://cdn.jsdelivr.net/gh/nataliojunior-pareto/consentstate@main/src/bridge21.js';
 
 async function initializeCMP() {
     try {
